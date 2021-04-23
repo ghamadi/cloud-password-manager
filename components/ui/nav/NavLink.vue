c730b406864828f1deb67f3a5c3c67df461ed5c5
Ghaleb Hamadi <gah09@mail.aub.edu>
2021-04-23 12:59:39 +0300

    feat: nav-link component
    A custom component to replace the v-list-item for more control over styling

---
+++ components/ui/nav/NavLink.vue
@@ -0,0 +1,54 @@
+<template>
+  <nuxt-link
+    class="nav-link d-flex align-center"
+    exact
+    exact-active-class="active-nav-link"
+    :to="to"
+  >
+    <v-icon size="20" class="mx-5"> {{ icon }} </v-icon>
+    <span> {{ title }} </span>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: [String, Object],
+      default: '',
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+a.nav-link {
+  text-decoration: none;
+  height: 35px;
+  font-size: 0.85em;
+
+  &:hover {
+    background-color: #f5f5f517;
+  }
+
+  & * {
+    color: white;
+  }
+
+  &.active-nav-link {
+    background-color: #ffffff29;
+    &,
+    & > * {
+      color: #ff6b5d;
+    }
+  }
+}
+</style>
