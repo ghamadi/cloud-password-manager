c1a1b50306c56d457c07b93986b50da0296a5e25
Ghaleb Hamadi <gah09@mail.aub.edu>
2021-04-24 13:23:05 +0300

    fix: remove unnecessary state keys from dialogs store

--- store/dialogs.js
+++ store/dialogs.js
@@ -1,21 +1,11 @@
 import { AlertObject } from '~/lib/models/alert_data'
 
 const state = () => ({
-  planPopupForm: false,
-  programPopupForm: false,
   alertDialog: false,
   currentAlert: {},
 })
 
 const getters = {
-  planPopupForm(state) {
-    return state.planPopupForm
-  },
-
-  programPopupForm(state) {
-    return state.programPopupForm
-  },
-
   alertDialog(state) {
     return state.alertDialog
   },
@@ -28,14 +18,6 @@ const getters = {
 const actions = {}
 
 const mutations = {
-  SET_PLAN_POPUP_FORM(state, value) {
-    state.planPopupForm = value
-  },
-
-  SET_PROGRAM_POPUP_FORM(state, value) {
-    state.programPopupForm = value
-  },
-
   SET_ALERT_DIALOG(state, value) {
     state.alertDialog = value
   },
